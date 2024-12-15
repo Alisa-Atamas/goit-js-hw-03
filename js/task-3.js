0bf14b677a0408a628e6a5ddefad36254e72ad8e
Alisa <alisaatamas92@gmail.com>
2024-12-15 20:17:09 +0200

    add index file

--- js/task-3.js
+++ js/task-3.js
@@ -1,19 +1,20 @@
-function checkForSpam(message) {
-  const lowerCaseMessage = message.toLowerCase();
-  
-  
-  if (lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale')) {
-    return true;  
-  }
-  
-  return false; 
-}
+function  filterArray(numbers, value):
+    # Створюємо порожній масив для зберігання підходящих чисел
+    filtered_numbers = []
+    
+    # Ітеруємо через кожен елемент масиву numbers
+    for number in numbers:
+        # Якщо число більше за value, додаємо його до нового масиву
+        if number > value:
+            filtered_numbers.append(number)
+    
+    # Повертаємо новий масив з відфільтрованими числами
+    return filtered_numbers
 
+    
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
